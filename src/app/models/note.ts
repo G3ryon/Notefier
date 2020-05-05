6c033386219eac1068636273803333d04519daa3
Theo <Dtheo98@gmail.com>
2020-05-05 14:49:15 +0200

    List Notes view
    +Adding the api services
    +Adding the display of the notes on the note-list view

---
+++ src/app/models/note.ts
@@ -0,0 +1,9 @@
+export class Note {
+    id:number;
+    title: string;
+    content: string;
+    date: Date;
+    valid:boolean;
+    category:JSON;
+
+}
--- src/app/note-list/note-list.page.ts
+++ src/app/note-list/note-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-note-list',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteListPage implements OnInit {
 
-  constructor() { }
+  notesData: any;
+
+  constructor(
+    public apiService: ApiService
+  ) { 
+    this.notesData = [];
+  }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    // Used ionViewWillEnter as ngOnInit is not 
+    // called due to view persistence in Ionic
+    this.getAllNotes();
+  }
+
+  getAllNotes() {
+    //Get saved list of notes
+    this.apiService.getList().subscribe(response => {
+      console.log(response);
+      this.notesData = response;
+    })
+  }
+
+  delete(item) {
+    //Delete item in Student data
+    this.apiService.deleteItem(item.id).subscribe(Response => {
+      //Update list after delete is successful
+      this.getAllNotes();
+    });
   }
 
 }
